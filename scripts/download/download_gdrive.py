6d3f1783a79cc8ebf1d74e3877cd31c0e4ade632
Dmitry Bogdanov <dmitry.bogdanov@upf.edu>
2019-06-06 14:18:54 +0200

    Add script for data downloads from Google Drive

---
+++ scripts/download/download_gdrive.py
@@ -0,0 +1,121 @@
+import argparse
+import sys
+import os.path
+import csv
+import hashlib
+import tarfile
+from pathlib import Path
+import gdown
+
+
+base_path = Path(__file__).parent
+ID_FILE_PATH = (base_path / "../../data/download/").resolve()
+
+
+def compute_sha256(filename):
+    with open(filename, 'rb') as f:
+        contents = f.read()
+        checksum = hashlib.sha256(contents).hexdigest()
+        return checksum
+    return None
+    
+    raise Exception('Error computing a checksum for %s' % filename)
+
+
+def download(dataset, data_type, output_dir, unpack_tars, remove_tars):
+    if not os.path.exists(output_dir):
+        sys.stderr.write('Output directory %s does not exist' % output_dir)
+        return
+
+    print('Downloading %s' % dataset)
+    file_gids = os.path.join(ID_FILE_PATH, dataset + '_' + data_type + '_gids.txt')
+    file_sha256_tars = os.path.join(ID_FILE_PATH, dataset + '_' + data_type + '_sha256_tars.txt')
+    file_sha256_tracks = os.path.join(ID_FILE_PATH, dataset + '_' + data_type + '_sha256_tracks.txt')
+
+    # Read checksum values for tars and files inside.
+    with open(file_sha256_tars) as f:
+        sha256_tars = dict([(row[1], row[0]) for row in csv.reader(f, delimiter=' ')])
+
+    with open(file_sha256_tracks) as f:
+        sha256_tracks = dict([(row[1], row[0]) for row in csv.reader(f, delimiter=' ')])
+
+    # Read google IDs for download.
+    ids = {}
+    with open(file_gids, 'r') as f:
+        for line in f:
+            id, filename = line.split(('   '))[:2]
+            ids[filename] = id
+
+    removed = []
+    for filename in ids:
+        output = os.path.join(output_dir, filename)
+
+        # Download from Google Drive.
+        if os.path.exists(output):
+            print('Skipping %s (file already exists)' % output)
+            continue
+        url = 'https://drive.google.com/uc?id=%s' % ids[filename]
+        gdown.download(url, output, quiet=False)
+
+        # Validate the checksum.
+        if compute_sha256(output) != sha256_tars[filename]:
+            sys.stderr.write('%s does not match the checksum, removing the file' % output)
+            removed.append(filename)
+            os.remove(output)
+        else:
+            print('%s checksum OK' % filename)
+
+    if removed:
+        print('Missing files:', ' '.join(removed))
+        print('Re-run the script again')
+        return
+
+    print('Download complete')
+
+    if unpack_tars:
+        print('Unpacking tar archives')
+
+        tracks_checked = []
+        for filename in ids:
+            output = os.path.join(output_dir, filename)
+            print('Unpacking', output)
+            tar = tarfile.open(output)
+            tracks = tar.getnames()[1:]  # The first element is folder name.
+            tar.extractall(path=output_dir)
+            tar.close()
+
+            # Validate checksums for all unpacked files
+            for track in tracks:
+                trackname = os.path.join(output_dir, track)
+                if compute_sha256(trackname) != sha256_tracks[track]:
+                    sys.stderr.write('%s does not match the checksum' % trackname)
+                    raise Exception('Corrupt file in the dataset: %s' % trackname)
+
+            print('%s track checksums OK' % filename)
+            tracks_checked += tracks
+            
+            if remove_tars:
+                os.remove(output)
+    
+    # Check if any tracks are missing in the unpacked archives.
+    if set(tracks_checked) != set(sha256_tracks.keys()):
+        raise Exception('Unpacked data contains tracks not present in the checksum files')
+
+    print('Unpacking complete')
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Download the MTG-Jamendo dataset from Google Drive',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+    parser.add_argument('--dataset', default='raw_30s', choices=['raw_30s', 'autotagging_moodtheme'],
+                        help='dataset to download')
+    parser.add_argument('--type', default='audio', choices=['audio', 'melspecs'],
+                        help='type of data to download (audio or mel-spectrograms)')
+    parser.add_argument('outputdir', help='directory to store the dataset')
+    parser.add_argument('--unpack', action='store_true', help='unpack tar archives')
+    parser.add_argument('--remove', action='store_true', help='remove tar archives while unpacking one by one (use to save disk space)')
+
+    args = parser.parse_args()
+    download(args.dataset, args.type, args.outputdir, args.unpack, args.remove)
