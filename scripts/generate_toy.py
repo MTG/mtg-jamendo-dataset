2f7aa3953621c0428800ad83c7eec866257cddaa
Philip Tovstogan <phil.tgun@gmail.com>
2021-03-04 11:05:59 +0100

    Generate toy subset with one track per album

--- scripts/generate_toy.py
+++ scripts/generate_toy.py
@@ -5,27 +5,30 @@ import commons
 import statistics
 
 
-def generate_toy(tracks: dict) -> dict:
+def generate_toy(tracks: dict, filter: str) -> dict:
     tracks_toy = {}
-    artist_ids = set()
+    filter_ids = set()
     track_ids = list(tracks.keys())
     random.shuffle(track_ids)
 
     for track_id in track_ids:
         track = tracks[track_id]
-        if track['artist_id'] not in artist_ids:
+        if track[f'{filter}_id'] not in filter_ids:
             tracks_toy[track_id] = track
-            artist_ids.add(track['artist_id'])
+            filter_ids.add(track[f'{filter}_id'])
 
     return tracks_toy
 
 
 if __name__ == '__main__':
-    parser = argparse.ArgumentParser(description='Generates toy subset by selecting one track per artist')
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+                                     description='Generates toy subset by selecting one track per artist')
     parser.add_argument('tsv_file', help=commons.METADATA_DESCRIPTION)
     parser.add_argument('output_prefix', help='output tsv file')
     parser.add_argument('--seed', type=int, default=0, help='randomization seed')
     parser.add_argument('--n', type=int, default=1, help='number of subsets to generate')
+    parser.add_argument('--filter', choices=['artist', 'album'], default='artist',
+                        help='the dataset will be generated with one track per entity that is indicated here')
     parser.add_argument('--stats-directory', default=None,
                         help='if this argument is set, statistics will be recomputed and written to this directory')
 
@@ -35,7 +38,7 @@ if __name__ == '__main__':
     tracks, tags, extra = commons.read_file(args.tsv_file)
     for i in range(args.n):
         print("Generating toy subset #{}".format(i))
-        tracks_toy = generate_toy(tracks)
+        tracks_toy = generate_toy(tracks, args.filter)
         filename = "{}_{}.tsv".format(args.output_prefix, i)
         commons.write_file(tracks_toy, filename, extra)
 
